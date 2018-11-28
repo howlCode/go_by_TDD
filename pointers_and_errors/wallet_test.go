cd7569bcbce664d26c4a6f08b4d2df359290f854
howlCode <miromanyth@gmail.com>
2018-11-28 09:23:18 -0500

    Finish Pointers & Errors Section 7

--- pointers_and_errors/wallet_test.go
+++ pointers_and_errors/wallet_test.go
@@ -2,46 +2,55 @@ package main
 
 import "testing"
 
+func assertBalance(t *testing.T, wallet Wallet, want BitCoin) {
+	t.Helper()
+	got := wallet.Balance()
+
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertError(t *testing.T, got error, want error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("wanted an error but did not get one")
+	}
+
+	if got != want {
+		t.Errorf("got '%s', want '%s'", got, want)
+	}
+}
+
+func assertNoError(t *testing.T, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
+	}
+}
+
 func TestWallet(t *testing.T) {
-
-	assertBalance := func(t *testing.T, wallet Wallet, want BitCoin) {
-		t.Helper()
-		got := wallet.Balance()
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	}
-
-	assertError := func(t *testing.T, got error, want error) {
-		t.Helper()
-		if got == nil {
-			t.Fatal("wanted an error but did not get one")
-		}
-
-		if got != want {
-			t.Errorf("got '%s', want '%s'", got, want)
-		}
-	}
 
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(BitCoin(10))
+
 		assertBalance(t, wallet, BitCoin(10))
 	})
 
 	t.Run("withdraw with funds", func(t *testing.T) {
 		wallet := Wallet{balance: BitCoin(20)}
-		wallet.Withdraw(BitCoin(10))
+		err := wallet.Withdraw(BitCoin(10))
+
 		assertBalance(t, wallet, BitCoin(10))
+		assertNoError(t, err)
 	})
 
 	t.Run("withdraw insufficent funds", func(t *testing.T) {
-		startingBalance := BitCoin(20)
-		wallet := Wallet{startingBalance}
+		wallet := Wallet{BitCoin(20)}
 		err := wallet.Withdraw(BitCoin(100))
 
-		assertBalance(t, wallet, startingBalance)
+		assertBalance(t, wallet, BitCoin(20))
 		assertError(t, err, ErrInsufficientFunds)
 
 	})
